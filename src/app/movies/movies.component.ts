a1096aa404a3e87daf049f502300f5861af507bf
harisaobad <harisaobad2@gmail.com>
2023-04-01 14:35:57 +0200

    new

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -15,6 +15,21 @@ export class MoviesComponent {
       Rating: '5',
       Opis: 'Kroz film se definitivno osjeća nostalgija za vremenima kada su snimani ovakvi filmovi, a sada je jedan takav naslov došao uz sve što nudi moderno kreiranje filmova.',
       Poster: 'assets/slikafilma.jpg',
+      Prikaz: true,
+    },
+    {
+      Naziv: 'Top Gun: Maverick',
+      Godina: '2022',
+      Zanr: 'Akacija',
+      Trajanje: '2h',
+      Rating: '5',
+      Opis: 'Kroz film se definitivno osjeća nostalgija za vremenima kada su snimani ovakvi filmovi, a sada je jedan takav naslov došao uz sve što nudi moderno kreiranje filmova.',
+      Poster: 'assets/slikafilma.jpg',
     },
   ];
+  showOpis: boolean = false;
+
+  toggleOpis() {
+    this.showOpis = !this.showOpis;
+  }
 }
